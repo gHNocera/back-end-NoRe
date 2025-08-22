665dee64df9eb8219ba44547ed2d569557fadbf8
gHartmann <hartmanngabrielno@outlook.com>
2025-08-22 18:13:11 -0300

    Alterações no PostController
    Agora o Post recebe JSON

--- nore/src/main/java/com/ghartmann/controller/PostController.java
+++ nore/src/main/java/com/ghartmann/controller/PostController.java
@@ -2,33 +2,61 @@ package com.ghartmann.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ghartmann.dao.IUserDAO;
+import com.ghartmann.dao.UserDAO;
 import com.ghartmann.domain.Post;
+import com.ghartmann.domain.PostDTO;
+import com.ghartmann.domain.User;
 import com.ghartmann.repository.PostRepository;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+    IUserDAO userDAO = new UserDAO();
 
     @Autowired
     private PostRepository postRepository;
 
     @PostMapping
-    public Post createPost(@RequestBody Post post) {
-        post.setTimeStamp(LocalDateTime.now());
+    public Post createPost(@RequestBody PostDTO postDTO) {
+        Post post = new Post();
+        post.setContent(postDTO.getContent());
+        post.setUsername(postDTO.getUsername());
+        post.setLikes(postDTO.getLikes());
+        post.setComentarios(postDTO.getComments());
+        post.setTimeStamp(
+        java.time.Instant.ofEpochMilli(postDTO.getTimestamp())
+                         .atZone(java.time.ZoneId.systemDefault())
+                         .toLocalDateTime()
+    );
         return postRepository.save(post);
+
     }
 
     @GetMapping
     public List<Post> getAllPosts() {
         return postRepository.findAll();
     }
+
+    @PostMapping("/{postId}/like/{userId}")
+    public Post likePost(@PathVariable Integer postId, @PathVariable Integer userId) {
+        Post post = postRepository.findById(postId);
+
+        User user = userDAO.getUserById(userId);
+
+        post.getLikedBy().add(user);
+        return postRepository.save(post);
+    }
+
 
 }
---
+++ nore/src/main/java/com/ghartmann/domain/PostDTO.java
@@ -0,0 +1,35 @@
+package com.ghartmann.domain;
+
+import java.util.List;
+
+public class PostDTO {
+    private Long id;
+    private String username;
+    private Long timestamp;
+    private String content;
+    private int likes;
+    private int comments;
+    private List<String> likesBy;
+
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public Long getTimestamp() { return timestamp; }
+    public void setTimestamp(Long timestamp) { this.timestamp = timestamp; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+
+    public int getLikes() { return likes; }
+    public void setLikes(int likes) { this.likes = likes; }
+
+    public int getComments() { return comments; }
+    public void setComments(int comments) { this.comments = comments; }
+
+    public List<String> getLikesBy() { return likesBy; }
+    public void setLikesBy(List<String> likesBy) { this.likesBy = likesBy; }
+}
